b529a489afc117cda65defb5301722737ef06e59
Alex Cattell <abcattell91@gmail.com>
2022-07-14 16:49:10 +0100

    finished props passing information from a parent to child elements

--- src/index.js
+++ src/index.js
@@ -6,14 +6,43 @@ import CommentDetail from './CommentDetail'
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author={faker.name.firstName()}/>
-      <CommentDetail author={faker.name.firstName()}/>
-      <CommentDetail author={faker.name.firstName()}/>
-      <CommentDetail author={faker.name.firstName()}/>
-      <CommentDetail author={faker.name.firstName()}/>
-      <CommentDetail author={faker.name.firstName()}/>
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 4:45PM"
+        comment="Great post!"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 5:45PM"
+        comment="Seriously amazing!"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Yesterday at 2:45PM"
+        comment="Great post!"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 1:45PM"
+        comment="Seriously amazing!"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Yesterday at 7:45PM"
+        comment="Great post!"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 3:45PM"
+        comment="Love it!"
+        avatar={faker.image.image()}
+      />
     </div>
-
   );
 };
 
