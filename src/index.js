3ffae52650bcca56a01a9c181c063bad8f770ef2
Alex Cattell <abcattell91@gmail.com>
2022-07-14 17:06:41 +0100

    More prop styling and arranging with a card along with buttons

--- src/index.js
+++ src/index.js
@@ -2,46 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author={faker.name.firstName()}
-        timeAgo="Today at 4:45PM"
-        comment="Great post!"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          timeAgo="Today at 4:45PM"
+          comment="Great post!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
       <CommentDetail
         author={faker.name.firstName()}
         timeAgo="Today at 5:45PM"
         comment="Seriously amazing!"
         avatar={faker.image.image()}
       />
+      </ApprovalCard>
+      <ApprovalCard>
       <CommentDetail
         author={faker.name.firstName()}
         timeAgo="Yesterday at 2:45PM"
         comment="Great post!"
         avatar={faker.image.image()}
       />
+      </ApprovalCard>
+      <ApprovalCard>
       <CommentDetail
         author={faker.name.firstName()}
         timeAgo="Today at 1:45PM"
         comment="Seriously amazing!"
         avatar={faker.image.image()}
       />
+      </ApprovalCard>
+      <ApprovalCard>
       <CommentDetail
         author={faker.name.firstName()}
         timeAgo="Yesterday at 7:45PM"
         comment="Great post!"
         avatar={faker.image.image()}
       />
+      </ApprovalCard>
+      <ApprovalCard>
       <CommentDetail
         author={faker.name.firstName()}
         timeAgo="Today at 3:45PM"
         comment="Love it!"
         avatar={faker.image.image()}
       />
+      </ApprovalCard>
     </div>
   );
 };
